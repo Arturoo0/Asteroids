dd401f18bd1b0e8ab4be1ae7a72a5c8465adf9c9
Arturoo0 <arturoportelles@gmail.com>
2020-10-31 16:17:40 -0400

    added simple intertia mechanic

--- src/Player.py
+++ src/Player.py
@@ -6,10 +6,13 @@ import Projectile
 class Player:
     def __init__(self, displayCtx, velocity):
         self.projectile = Projectile.Projectile(displayCtx)
-        self.velocity = velocity
+        self.xVelocity = 0
+        self.yVelocity = 0
         self.displayCtx = displayCtx
         self.polygonRepresentation = [[30, 30], [40, 60], [20, 60]]
         self.speed = 30
+        self.deceleration = .99
+        self.rotationSpeed = 1.5
         self.keysHeld = {
             'a' : False, 
             'w' : False,
@@ -22,6 +25,7 @@ class Player:
 
     def update(self, dt):
         self.computeMovement(dt)
+        self.applyInertia(dt)
     
     def trackKeyPresses(self, event):
         if event.type == pygame.KEYDOWN:
@@ -37,9 +41,9 @@ class Player:
     def computeMovement(self, dt):
         for k,v in self.keysHeld.items():
             if v:
-                if k == 'a': self.rotate(self.computeCentroid(), -1.5 * dt)
+                if k == 'a': self.rotate(self.computeCentroid(), -self.rotationSpeed * dt)
                 if k == 'w': self.shiftPlayerForward(self.getPlayerAngle(), dt)
-                if k == 'd': self.rotate(self.computeCentroid(), 1.5 * dt)
+                if k == 'd': self.rotate(self.computeCentroid(), self.rotationSpeed * dt)
 
     def computeCentroid(self):
         centroidX = 0
@@ -69,9 +73,25 @@ class Player:
     
     def shiftPlayerForward(self, angle, dt):
         for point in self.polygonRepresentation:
-            point[0] += (self.speed * dt) * math.cos(angle)
-            point[1] += (self.speed * dt) * math.sin(angle)
+            dx = (self.speed * dt) * math.cos(angle)
+            dy = (self.speed * dt) * math.sin(angle)
+            point[0] += dx 
+            point[1] += dy
 
+            self.xVelocity = dx * 20
+            self.yVelocity = dy * 20
+    
+    def applyInertia(self, dt):
+        if not self.keysHeld['w'] and (abs(self.xVelocity > 0) or abs(self.yVelocity) > 0):
+            for point in self.polygonRepresentation:
+                point[0] += self.xVelocity * dt
+                point[1] += self.yVelocity * dt
+            
+            if abs(self.xVelocity) < 1: self.xVelocity = 0 
+            if abs(self.yVelocity) < 1: self.yVelocity = 0 
+
+            self.xVelocity *= self.deceleration
+            self.yVelocity *= self.deceleration
 
     
     
